e9889a1cdab3bfe5109eb06c7194b8d4cf04b76a
sandajama <sandajama29@gmail.com>
2023-06-21 14:21:34 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function count(letter,str){
+    let total=0
+
+    str.split("").forEach(char=> {
+        if(char === letter){
+            total++
+        }
+    });
+    return total
+    
+    }
+    
+//const result =count("c", "bootcamp")
+const result =count("o", "fikrcamp coding bootcamp")
+//const result =count("a","fikrcamp") 
+console.log(result)
